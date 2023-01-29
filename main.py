a1255e1327d165f424d0feccba5faae4917f5c54
ayychapin <allison.chapin@gmail.com>
2023-01-28 20:47:41 -0700

    Fixed math for beatmap generation.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import librosa
-import synth_mapping_helper.synth_format as sf
 
 import numpy as np
+from scipy import stats as st
 import json
 import matplotlib.pyplot as plt
 import mplcyberpunk
 
 class SoundFile:
-    def __init__(self, filename, bpm=100, offset=0):
+    def __init__(self, filename, bpm=None, offset=0):
         '''
 
         :param filename: path to audio file
@@ -16,8 +16,12 @@ class SoundFile:
         '''
         self.y, self.samplingrate = librosa.load(filename)
 
-        self.bpm = bpm
-        self.increment = self.bpm * 64 / 60  #increments per sec
+        if bpm==None:
+            self.bpm = self.tempos()
+        else:
+            self.bpm = bpm
+
+        self.increment = self.bpm * 64 / 60  #1/64th increments per sec
         self.offset = offset / 1000
         self.timestamps = None
         self.beats()
@@ -26,15 +30,32 @@ class SoundFile:
         '''
         Convert the timestamps to beatmap timings
         z=(Position/64)(60/bpm)2*10, where Position is the ordinal count of 1/64 increments since the song start
-
+        From emikodo: The time is the measure * (60/bpm * 1000).  The start time is 60/bpm * 2 * 10 * startMeasure
         :time_elapsed: float or numpy array, actual time in audio file
-        :return: z coordinate
+        :return: positions in 1/64 increments, z coordinates
         '''
 
-        position = (time_elapsed - self.offset) * self.increment
+        # convert seconds to number of increments
+        position = np.round((time_elapsed - self.offset) * self.increment).astype(int).astype(str) # convert seconds to number of increments
+        # calculate the z's
         z = (time_elapsed - self.offset) * 2 * 10
 
-        return z
+        return position, z
+
+    def tempos(self, chart=False):
+        onset_env = librosa.onset.onset_strength(y=self.y, sr=self.samplingrate)
+        # find the dynamic tempo
+        dtempo = librosa.beat.tempo(onset_envelope=onset_env, sr=self.samplingrate, aggregate=None)
+
+        if chart:
+            plt.style.use("cyberpunk")
+            plt.plot(dtempo)
+            plt.xlim([0, librosa.get_duration(y=self.y, sr=self.samplingrate)])
+            plt.title("Detected BPM")
+            plt.show()
+
+        # and return the mode
+        return st.mode(dtempo).mode[0]
 
     def beats(self):
         pulse_array = librosa.beat.plp(y=self.y, sr=self.samplingrate)
@@ -43,19 +64,12 @@ class SoundFile:
         onset_env = librosa.onset.onset_strength(y=self.y, sr=self.samplingrate)
         times = librosa.times_like(onset_env, sr=self.samplingrate)
 
-        # plt.style.use("cyberpunk")
-        # plt.plot(times, librosa.util.normalize(pulse_array),
-        #          label='Wavy')
-        # plt.vlines(times[beats_plp], 0, 1, alpha=0.5,
-        #            linestyle='--', label='Local Beats')
-        # plt.legend()
-        # plt.xlim([130, 140])
-        # plt.show()
-
         self.timestamps = times[beats_plp] # should be timestamps in seconds
 
     def generate_beatmap(self, path):
-
+        positions, z_vals = self.time_to_synthmap(self.timestamps)
+        # TODO: should Segments be None or "null"?
+        notes = [[{"Position": [0, 0, z], "Segments": None, "Type": 0}] for z in z_vals]
 
         # format yoinked from synth_mapping_helper <3
         # https://github.com/adosikas/synth_mapping_helper/blob/main/src/synth_mapping_helper/synth_format.py
@@ -63,8 +77,8 @@ class SoundFile:
             "BPM": self.bpm,
             "startMeasure": 0,
             "startTime": 0,
-            "lenght": 0,
-            "notes": {},
+            "lenght": self.timestamps[-1] * 1000,  # largest time value in milliseconds
+            "notes": dict(zip(positions, notes)),
             "effects": [],
             "jumps": [],
             "crouchs": [],
@@ -74,18 +88,13 @@ class SoundFile:
             "lights": [],
         }
 
-        # convert to z values for the beatmap
-        z_vals = [self.time_to_synthmap(x) for x in self.timestamps]
+        # Old stuff, bad math, do not use.
         # this will generate a list of dicts with the correct note format
-        notes = [sf.note_to_synth(self.bpm, 1, np.array([0,0,z]).reshape(1,3)) for z in z_vals]
-
+        # notes = [sf.note_to_synth(self.bpm, 1, np.array([0,0,z]).reshape(1,3)) for z in z_vals]
         # again, adapted from synth_mapping_helper
-
-        first = 99999
-        last = -99999
         # wrong note storage format?
-        for note in notes:
-            beatmap["notes"][round(note['Position'][2] * 64)] = [note]
+        # for note in notes:
+        #     beatmap["notes"][round(note['Position'][2] * 64)] = [note]
 
         json_string = json.dumps(beatmap)
         with open(path, "w") as outfile:
@@ -97,6 +106,5 @@ if __name__ == '__main__':
 
     winter = SoundFile("Vivaldi_L'inverno_Allegro_Non_Molto.ogg")
     winter.generate_beatmap("generated_beatmap.json")
-
 
     print()
