e3b6bf2c921167ce63a2e319add90de43a1dd9ee
Allison <allison.chapin@gmail.com>
2023-03-20 21:46:38 -0600

    Join all rails and split to 10 second rails functions

--- synth_auto_map/audio_trip_conversion.py
+++ synth_auto_map/audio_trip_conversion.py
@@ -1,4 +1,7 @@
+from bisect import bisect
 import json
+import math
+import numpy as np
 
 '''Functions for conversion from Audio Trip .ats files to Synth Riders compatible JSON.
     This will convert gems and ribbons ONLY.
@@ -40,18 +43,141 @@ def xy_ats_to_synth(x, y):
 
     return xs, ys
 
-def all_rails():
-    ''''''
+
+def interpolate_at_time(point1, point2, t):
+    """find xyz position at time t given two points (x, y, t)"""
+    scaled_vector = (point2[:2] - point1[:2]) * (t - point1[2]) / (point2[2] - point1[2])
+    xy = (point1[:2] + scaled_vector).tolist()
+    xy.append(t)
+    return xy
+
+
+def rail_format(rail_array, type):
+    """Convert an array of positions to a rail dictionary"""
+    return {"Position": rail_array[0], "Segments": rail_array[1:], "Type": type}
+
+
+def all_rails(synth_json):
+    """Connect all notes into rails"""
+
+    notes = synth_json['notes']
+    position_types = {k: {n['Type']: {'Position': n['Position'], 'Segments': n['Segments']} for n in notes[k]} for k in
+                      notes.keys()}
+    note_types = [0, 1, 2, 3]
+    # for the new rail associated with each note type
+    new_rails = {}
+    # position keys for each note type
+    first_positions = {}
+
+    for type in note_types:
+        # get keys for note type
+        positions = [k for k in position_types.keys() if type in position_types[k].keys()]
+        # get a list of all positions for each note type
+        position_list = [[position_types[k][type]['Position']] + position_types[k][type]['Segments']
+                         if position_types[k][type]['Segments'] is not None else [position_types[k][type]['Position']]
+                         for k in positions]
+
+        position_list = [n for p in position_list for n in p]
+        if len(position_list) > 0:
+            new_rails[type] = {'Position': position_list[0], 'Segments': None, 'Type': type}
+            first_positions[type] = positions[0]
+
+            if len(position_list) > 1:
+                new_rails[type]['Segments'] = position_list[1:]
+
+    # The note types we actually have
+    final_types = list(new_rails.keys())
+    new_json = synth_json.copy()
+
+    # the first rail
+    new_json['notes'] = {first_positions[final_types[0]]: [new_rails[final_types[0]]]}
+
+    # if we actually have more rails...
+    if len(final_types) > 1:
+        for type in final_types[1:]:
+            p = first_positions[type]
+            if p in new_json['notes'].keys():
+                new_json['notes'][p].append(new_rails[type])
+            else:
+                new_json['notes'][p].append(new_rails[type])
+
+    # validate rails and split
+    return validate_rails(new_json)
+
 
 def validate_rails(synth_json):
-    ''''''
+    """Check all rails in the beatmap and split any that are longer than 10 seconds"""
 
-def ats_to_synth(path, choreo_number=None):
+    increment = synth_json['BPM'] * 64 / 60 / 20
+
+    new_json = synth_json.copy()
+    new_json['notes'] = {}
+
+    for position in synth_json['notes'].keys():
+        if position not in new_json['notes']:
+            new_json['notes'][position] = []
+        for type in synth_json['notes'][position]:
+            # single notes
+            if type['Segments'] is None:
+                new_json['notes'][position].append(type)
+            # rails
+            else:
+                length = type['Segments'][-1][2] - type['Position'][2]
+                if length < 200:
+                    # short rail
+                    new_json['notes'][position].append(type)
+                else:
+                    # long rail
+                    # get split time positions, use slightly larger value for safety
+                    num_splits = math.ceil(length / 205)
+                    split_times = np.linspace(type['Position'][2], type['Segments'][-1][2], num=num_splits + 2)[1:-1]
+                    # Find insertion points: https://docs.python.org/3/library/bisect.html#bisect.bisect
+                    segment_times = [n[2] for n in type['Segments']]
+                    insertion_indices = np.array([bisect(segment_times, s) + 1 for s in split_times])
+
+                    split_indices = []
+                    old_segments = [type['Position']] + type['Segments']
+
+                    for n in range(split_times.size):
+                        idx = insertion_indices[n]
+                        split_xyz = interpolate_at_time(np.array(old_segments[idx - 1]), np.array(old_segments[idx]),
+                                                        split_times[n])
+                        # insert the new point
+                        old_segments.insert(idx, split_xyz)
+                        # record the split location in the list
+                        split_indices.append(idx)
+                        # add 1 to remaining insertion indices
+                        insertion_indices[n + 1:] += 1
+
+                    # Now, actually split the thing
+                    # The first one is pretty straightforward
+                    new_json['notes'][position].append(rail_format(old_segments[:split_indices[0] + 1], type["Type"]))
+                    # The others require some checks
+                    for n in range(len(split_indices) - 1):
+                        new_rail = rail_format(old_segments[split_indices[n]: split_indices[n + 1] + 1], type["Type"])
+                        step = round(old_segments[split_indices[n]][2] * increment)
+                        if step not in new_json['notes']:
+                            new_json['notes'][step] = [new_rail]
+                        else:
+                            new_json['notes'][step].append(new_rail)
+                    # and the last one...
+                    last_rail = rail_format(old_segments[split_indices[-1]:], type["Type"])
+                    step = round(old_segments[split_indices[-1]][2] * increment)
+                    if step not in new_json['notes']:
+                        new_json['notes'][step] = [last_rail]
+                    else:
+                        new_json['notes'][step].append(last_rail)
+
+    return new_json
+
+
+def ats_to_synth(path, choreo_number=None, convert_to_rails=False):
     '''
     Import an audio trip ats file to synth riders (basic gems/notes and ribbons/rails only)
 
     :path: path to .ats file containing Audio Trip choreography
     :choreo_number: index (starting at 0) of Audio Trip choreography to use, if known
+    :convert_to_rails: boolean, set to True to join all notes into rails
     :return: dict of things ready to be converted to Synth Riders json
     '''
     f = open(path)
@@ -65,7 +191,6 @@ def ats_to_synth(path, choreo_number=None):
                    if section['doesStartNewMeasure']]
     offset = offset_list[0]
     offset_inc = round(offset * increment)
-
 
     synth_json = {
         "BPM": bpm,
@@ -81,7 +206,6 @@ def ats_to_synth(path, choreo_number=None):
         "slides": [],
         "lights": [],
     }
-
 
     '''Dictionary for mapping from gems (AT) to notes (SR)
             right gems type 2, right ribbons type 4
@@ -124,7 +248,7 @@ def ats_to_synth(path, choreo_number=None):
             for node in gem['subPositions'][1:]:
                 x_offset, y_offset = xy_ats_to_synth(node['x'], node['y'])
                 z_val += step
-                new_note["Segments"].append([x+x_offset, y+y_offset, z_val])
+                new_note["Segments"].append([x + x_offset, y + y_offset, z_val])
 
             # Add to an existing position if one already exists
             if s_position in synth_json['notes']:
@@ -142,4 +266,7 @@ def ats_to_synth(path, choreo_number=None):
             else:
                 synth_json['notes'][s_position] = [new_note]
 
-    return synth_json
+    if convert_to_rails:
+        return all_rails(synth_json)
+    else:
+        return validate_rails(synth_json)
