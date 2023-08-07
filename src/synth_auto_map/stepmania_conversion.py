30bd4b996d67ab177f1d778ea929498a9a87e9a4
ayychapin <allison.chapin@gmail.com>
2023-08-07 09:06:31 -0600

    start stepmania

---
+++ src/synth_auto_map/stepmania_conversion.py
@@ -0,0 +1,8 @@
+import re
+
+# https://step-mania.fandom.com/wiki/.sm#:~:text=sm%20is%20one%20of%20the,chart%20data%20in%20the%20file
+# https://github-wiki-see.page/m/stepmania/stepmania/wiki/sm
+
+
+#bpm and offset are included!
+# get out measure chunks
